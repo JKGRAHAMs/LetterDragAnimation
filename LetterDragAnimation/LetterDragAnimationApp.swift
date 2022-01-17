da5a199a2e16a6cf5952486f25387d71fdf6359d
Joey Graham <82058059+JKGRAHAMs@users.noreply.github.com>
2022-01-17 13:30:06 -0600

    Initial Commit

---
+++ LetterDragAnimation/LetterDragAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterDragAnimationApp.swift
+//  LetterDragAnimation
+//
+//  Created by Joey Graham on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct LetterDragAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
